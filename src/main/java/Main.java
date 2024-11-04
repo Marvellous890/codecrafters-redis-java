a8b0b9d907bc8e45e9f71666147c9002edb88448
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-11-04 15:02:58 +0100

    respond to multiple pings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,7 +15,8 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+
+            process(clientSocket);
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
@@ -26,6 +27,26 @@ public class Main {
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
             }
+        }
+    }
+
+    private static void process(Socket clientSocket) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+             BufferedWriter writer = new BufferedWriter(
+                     new OutputStreamWriter(clientSocket.getOutputStream()));) {
+            String content;
+            while ((content = reader.readLine()) != null) {
+                System.out.println("::" + content);
+                if ("ping".equalsIgnoreCase(content)) {
+                    writer.write("+PONG\r\n");
+                    writer.flush();
+                } else if ("eof".equalsIgnoreCase(content)) {
+                    System.out.println("eof");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
