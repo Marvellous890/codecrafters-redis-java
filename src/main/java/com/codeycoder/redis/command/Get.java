34d9b54aeaed7ec466673f9fe6cf23cbc8761992
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-11-11 11:18:18 +0100

    Read multiple string values

--- src/main/java/com/codeycoder/redis/command/Get.java
+++ src/main/java/com/codeycoder/redis/command/Get.java
@@ -1,23 +1,31 @@
 package com.codeycoder.redis.command;
 
+import com.codeycoder.redis.config.Logger;
 import com.codeycoder.redis.config.ObjectFactory;
 import com.codeycoder.redis.storage.Storage;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
 
 public class Get extends AbstractHandler {
+    private static final Logger LOGGER = new Logger(Get.class);
+
     public Get(ObjectFactory objectFactory) {
         super(objectFactory);
     }
 
     @Override
     public byte[] handle(String[] arguments) {
+        LOGGER.log("Arguments: " + Arrays.toString(arguments));
         String value;
         if (objectFactory.getApplicationProperties().getDir() == null) {
             value = Storage.get(arguments[1]);
         } else {
             try {
-                value = new String(objectFactory.getRdbProcessor().readFirstKeyValuePair().getValue());
+                Map<String, String> pairs = objectFactory.getRdbProcessor().readAllPairs();
+                LOGGER.log("Pairs read: " + pairs);
+                value = pairs.get(arguments[1]);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
